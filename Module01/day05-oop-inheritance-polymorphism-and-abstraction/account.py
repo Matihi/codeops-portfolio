1ca99a19e27051c872a96ca8f77c450308015f05
Matihi <mesfinnatitik@gmail.com>
2026-07-15 20:58:58 +0300

    do in-class exercise

--- Module01/day05-oop-inheritance-polymorphism-and-abstraction/account.py
+++ Module01/day05-oop-inheritance-polymorphism-and-abstraction/account.py
@@ -9,23 +9,76 @@ class Account:
     @property
     def balance(self):
         return self.__balance
+    
+    #we need to add this method so that withdraw of CurrentAccount can set balance, since it does
+    # not have the permission to access self.__balance of Account class because we set a read-only property with no setter.
+    def _change_balance(self, amount):
+        self.__balance += amount
 
     def deposite(self, amount):
         if amount <= 0:
             raise ValueError("Amount must be positive")
-        self.__balance += amount  #If we had a setter method as well and it was like this: self.balance = self.balance + amount it means
-                                                                                 #then calls the setter method second =  calls the getter method first + amount(adds amount)
+        self._change_balance(amount)
+
     def withdraw(self, amount):
         if amount <= 0:
             raise ValueError("Amount must be positive")
         elif self.balance < amount:
             raise ValueError("Balance not sufficient")
         else:
-            self.__balance -= amount
+            self._change_balance(amount)
+            #self.__balance -= amount # if it were this, we wouldn't be able to access a private property directly
+            #self.balance -= amount # if it were this, it would have thrown this: AttributeError: property 'balance' of 'CurrentAccount' object has no setter. We could create a setter if it were not a read-only property
 
     def statement(self):
-        print(f"Owner {self.owner} with account number {self.account_number} has balance {self.balance} ETB")
+        print(f"Owner {self.owner} with account number {self.account_number} has balance {self.balance} ETB\n")
 
 
+class SavingsAccount(Account):
+    def __init__(self, owner, account_number, balance=0, rate=0.05):
+        super().__init__(owner, account_number, balance)
+        self.rate = rate
 
-        
+    def add_interest(self):
+        self.deposite(self.balance * self.rate)
+
+    def statement(self):
+        print(f"Savings Account: {self.owner} with account number {self.account_number} and interest rate of {self.rate} has balance {self.balance} ETB\n")
+
+
+class CurrentAccount(Account):
+    def __init__(self, owner, account_number, balance=0, overdraft=1000):
+        super().__init__(owner, account_number, balance)
+        self.overdraft_limit = overdraft
+
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount must be positive")
+        elif self.balance < amount and abs(self.balance - amount) > self.overdraft_limit:
+            raise ValueError("Exceeds overdraft limit")
+        else:
+            self._change_balance(-amount)
+    
+    def statement(self):
+        print(f"Current Account: {self.owner} with account number {self.account_number} and an overdraft limit of {self.overdraft_limit} has balance {self.balance} ETB\n")
+
+account1 = Account("Abebe", "29485", 1800)
+account2 = Account("Kebede", "12957", 2300)
+savingsAccount1 = SavingsAccount("Almaz", "12345", 2000)
+savingsAccount2 = SavingsAccount("Lemlem", "56789", 3545, 0.1)
+currentAccount1 = CurrentAccount("Dawit", "34567", 2510)
+currentAccount2 = CurrentAccount("Abeje", "13467", 3200, 1500)
+
+account_list = [account1, account2, savingsAccount1, savingsAccount2, currentAccount1, currentAccount2]
+
+savingsAccount1.deposite(200)
+currentAccount1.deposite(300)
+currentAccount2.statement()
+currentAccount2.withdraw(4000)
+currentAccount2.statement()
+print("\n\n")
+
+for account in account_list:
+    account.statement()
+
