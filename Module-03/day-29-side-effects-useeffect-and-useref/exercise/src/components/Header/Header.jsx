33b025ad7ec2d79ad8b2f4f5f5b7d9a3d3d0e14a
Matihi <mesfinnatitik@gmail.com>
2026-09-03 01:42:52 +0300

    add focus to search box on mount with useRef

--- Module-03/day-29-side-effects-useeffect-and-useref/exercise/src/components/Header/Header.jsx
+++ Module-03/day-29-side-effects-useeffect-and-useref/exercise/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import SearchBox from "./SearchBox/SearchBox";
 import "./Header.css";
 
 function Header() {
   return (
     <header>
       <h1 className="heading">Addis Eats</h1>
+      <SearchBox />
     </header>
   );
 }
