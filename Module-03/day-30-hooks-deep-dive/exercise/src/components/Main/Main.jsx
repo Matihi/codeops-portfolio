bc2376741f428481f2f6aacdeafdbf95033bc08b
Matihi <mesfinnatitik@gmail.com>
2026-09-05 09:00:14 +0300

    copy exercise directory from day-29 to day-30

---
+++ Module-03/day-30-hooks-deep-dive/exercise/src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import Menu from "./Menu/Menu";
+import SideBar from "./SideBar/SideBar";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+      <Menu />
+      {/* <SideBar /> */}
+    </main>
+  );
+}
+
+export default Main;
