d52da0383e5ca33445bc74b762974b459197c125
Matihi <mesfinnatitik@gmail.com>
2026-09-05 21:03:27 +0300

    add Cart component to Main component

--- Module-03/day-30-hooks-deep-dive/exercise/src/components/Main/Main.jsx
+++ Module-03/day-30-hooks-deep-dive/exercise/src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import Menu from "./Menu/Menu";
 import SideBar from "./SideBar/SideBar";
 import "./Main.css";
+import Cart from "./Cart/Cart";
 
 function Main() {
   return (
     <main>
       <Menu />
+      <Cart />
       {/* <SideBar /> */}
     </main>
   );
