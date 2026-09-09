0fe2d2ede8a1aeb81e3685dabe21c8ff09de8abe
Matihi <mesfinnatitik@gmail.com>
2026-09-09 19:20:09 +0300

    make Home use DishList and useFetch

--- Module-03/day-31-react-router-v6/exercise/src/pages/Home/Home.jsx
+++ Module-03/day-31-react-router-v6/exercise/src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
+import DishList from "../../components/Main/Menu/DishList/DishList";
+import useFetch from "../../hooks/useFetch";
+
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const category = "All";
+  const url = "/data/dishes.json";
+  const { filteredDishes: dishes, loading, error } = useFetch(url, category);
+
+  if (loading) return <p>Loading special dishes...</p>;
+  if (error) return <p>{error}</p>;
+
+  const specialDishes = dishes.filter((dish) => dish.special === true);
+
+  const headingText = "Special Dishes";
+  const emptyMessage = "Sorry, special dishes are not available";
+
+  return (
+    <div className={styles.home}>
+      <h2 className={styles.homeHeading}>
+        Experience authentic and delicious Ethiopian cuisine
+      </h2>
+      <DishList
+        dishes={specialDishes}
+        headingText={headingText}
+        emptyMessage={emptyMessage}
+      />
+    </div>
+  );
 };
 
 export default Home;
