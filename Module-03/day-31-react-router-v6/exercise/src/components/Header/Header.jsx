e4645f0e09034d0260e14b5539650b03e5c388f3
Matihi <mesfinnatitik@gmail.com>
2026-09-10 18:49:40 +0300

    add Link to go to Register page

--- Module-03/day-31-react-router-v6/exercise/src/components/Header/Header.jsx
+++ Module-03/day-31-react-router-v6/exercise/src/components/Header/Header.jsx
@@ -9,6 +9,9 @@ function Header() {
       <Link to="/cart" className="badge-link">
         <CartBadge />
       </Link>
+      <Link to="/register" className="register-link">
+        Register
+      </Link>
       <h1 className="heading">Addis Eats</h1>
       <NavigationBar />
     </header>
