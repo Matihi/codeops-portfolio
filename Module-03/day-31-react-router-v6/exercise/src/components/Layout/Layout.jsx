e02093d22e6ec4b9351aee3c4539faccf0a97f8b
Matihi <mesfinnatitik@gmail.com>
2026-09-11 19:16:33 +0300

    make Layout handle auth loading state

--- Module-03/day-31-react-router-v6/exercise/src/components/Layout/Layout.jsx
+++ Module-03/day-31-react-router-v6/exercise/src/components/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "../../context/authentication/AuthProvider";
 import styles from "./Layout.module.css";
 
 const Layout = () => {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className={styles.structure}>
       <Header />
