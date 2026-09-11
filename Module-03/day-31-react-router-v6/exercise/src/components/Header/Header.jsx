4cd1523f64977607389d77a189087d9916af2d31
Matihi <mesfinnatitik@gmail.com>
2026-09-11 17:51:39 +0300

    make Header consume AuthContext and conditionally render login link

--- Module-03/day-31-react-router-v6/exercise/src/components/Header/Header.jsx
+++ Module-03/day-31-react-router-v6/exercise/src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import CartBadge from "./CartBadge/CartBadge";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/authentication/AuthProvider";
 import "./Header.css";
 
 function Header() {
+  const { user } = useAuth();
+
   return (
     <header className="header">
       <Link to="/cart" className="badge-link">
         <CartBadge />
       </Link>
-      <Link to="/register" className="register-link">
-        Register
-      </Link>
+      <div className="reg-and-auth-wrapper">
+        <Link to="/register" className="register-link">
+          Register
+        </Link>
+        {!user && (
+          <Link to="/login" className="login-link">
+            Login
+          </Link>
+        )}
+      </div>
+
       <h1 className="heading">Addis Eats</h1>
       <NavigationBar />
     </header>
