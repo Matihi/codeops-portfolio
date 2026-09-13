d7553ed0b7480dd152a6c022a72b98d50df254e5
Matihi <mesfinnatitik@gmail.com>
2026-09-13 16:11:44 +0300

    add conditional rendering to Cart page

--- Module-03/day-31-react-router-v6/exercise/src/pages/Cart/Cart.jsx
+++ Module-03/day-31-react-router-v6/exercise/src/pages/Cart/Cart.jsx
@@ -17,22 +17,32 @@ const Cart = () => {
     />
   ));
 
+  const cartCount = cartContextValue.cart.cartItems.length;
+
   return (
     <aside className={styles["cart-comp"]}>
       <h2>Your Order</h2>
-      <button
-        className={styles["clear-cart"]}
-        onClick={() => cartContextValue.dispatch({ type: "cart_cleared" })}
-      >
-        Clear Cart
-      </button>
-
-      <>{cartElements}</>
-      <p>Distinct Dishes: {String(cartContextValue.cart.cartItems.length)}</p>
-      <p>Total Dishes: {String(cartContextValue.totalItems)}</p>
-      <p>Total Price: {cartContextValue.totalPrice?.toLocaleString()} ETB</p>
-
-      <Link to="/checkout">Checkout</Link>
+      {cartCount === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <>
+          <button
+            className={styles["clear-cart"]}
+            onClick={() => cartContextValue.dispatch({ type: "cart_cleared" })}
+          >
+            Clear Cart
+          </button>
+          <>{cartElements}</>
+          <p>
+            Distinct Dishes: {String(cartContextValue.cart.cartItems.length)}
+          </p>
+          <p>Total Dishes: {String(cartContextValue.totalItems)}</p>
+          <p>
+            Total Price: {cartContextValue.totalPrice?.toLocaleString()} ETB
+          </p>
+          <Link to="/checkout">Checkout</Link>
+        </>
+      )}
     </aside>
   );
 };
