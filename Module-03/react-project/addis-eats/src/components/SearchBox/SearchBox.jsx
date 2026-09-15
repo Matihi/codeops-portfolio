9f5a116d4f69e438ec97e54dd0b0ac1b871e4085
Matihi <mesfinnatitik@gmail.com>
2026-09-15 12:27:36 +0300

    add SearchBox to Menu, make Menu pass state and updater to SearchBox and make SearchBox input a controlled input

--- Module-03/react-project/addis-eats/src/components/SearchBox/SearchBox.jsx
+++ Module-03/react-project/addis-eats/src/components/SearchBox/SearchBox.jsx
@@ -1,21 +1,28 @@
 import { useEffect, useRef } from "react";
 import "./SearchBox.css";
 
-const SearchBox = () => {
+const SearchBox = ({ query, setQuery }) => {
   const searchBoxRef = useRef(null);
 
   useEffect(() => {
     searchBoxRef.current.focus();
   }, []);
 
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <search className="search-container">
       <input
         ref={searchBoxRef}
         type="search"
+        name="search"
+        value={query}
         id="search"
         className="search"
         placeholder="Search dishes"
+        onChange={handleSearch}
       />
     </search>
   );
--- Module-03/react-project/addis-eats/src/pages/Menu/Menu.jsx
+++ Module-03/react-project/addis-eats/src/pages/Menu/Menu.jsx
@@ -1,7 +1,9 @@
 import CategoryBar from "../../components/CategoryBar/CategoryBar";
 import DishList from "../../components/DishList/DishList";
+import SearchBox from "../../components/SearchBox/SearchBox";
 import useFetch from "../../hooks/useFetch";
 import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 import styles from "./Menu.module.css";
 
 function Menu() {
@@ -9,9 +11,17 @@ function Menu() {
   const category = searchParams.get("category") ?? "All";
   const url = "https://addis-eats-backend.onrender.com/menu/";
   const { filteredDishes: dishes, loading, error } = useFetch(url, category);
+  const [query, setQuery] = useState("");
 
   if (loading) return <p>Loading the menu...</p>;
   if (error) return <p>{error}</p>;
+
+  const searchQuery = query.trim().toLowerCase();
+  const shown = dishes.filter(
+    (dish) =>
+      dish?.nameEn?.toLowerCase().includes(searchQuery) ||
+      dish?.nameAm?.toLowerCase().includes(searchQuery),
+  );
 
   const headingText = "Our Menu";
   const emptyMessage = "Sorry, dishes in this category are not available";
@@ -23,8 +33,9 @@ function Menu() {
         selected={category}
         onSelect={setSearchParams}
       />
+      <SearchBox query={query} setQuery={setQuery} />
       <DishList
-        dishes={dishes}
+        dishes={shown}
         headingText={headingText}
         emptyMessage={emptyMessage}
       />
