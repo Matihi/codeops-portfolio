f01de25c27c91c654b5d96bfdca20c0465d436d3
Matihi <mesfinnatitik@gmail.com>
2026-09-20 19:24:41 +0300

    make cart data persistent by adding it to local storage

--- Module-03/react-project/addis-eats/src/context/cart/CartProvider.jsx
+++ Module-03/react-project/addis-eats/src/context/cart/CartProvider.jsx
@@ -1,10 +1,38 @@
-import { createContext, useMemo, useReducer } from "react";
+import { createContext, useMemo, useReducer, useEffect } from "react";
 import cartReducer from "./cartReducer";
 
 export const CartContext = createContext(null);
 
+const STORAGE_KEY = "addis-eats-cart";
+
+const initialCart = { cartItems: [] };
+
+const initializeCart = (defaultCart) => {
+  const storedCart = localStorage.getItem(STORAGE_KEY);
+
+  if (!storedCart) {
+    return defaultCart;
+  }
+
+  try {
+    return JSON.parse(storedCart);
+  } catch (error) {
+    console.log(error);
+    return defaultCart;
+  }
+};
+
 const CartProvider = ({ children }) => {
-  const [cart, dispatch] = useReducer(cartReducer, { cartItems: [] });
+  const [cart, dispatch] = useReducer(cartReducer, initialCart, initializeCart);
+
+  useEffect(() => {
+    try {
+      const cartString = JSON.stringify(cart);
+      localStorage.setItem(STORAGE_KEY, cartString);
+    } catch (error) {
+      console.log("Could not save cart to localStorage:", error);
+    }
+  }, [cart]);
 
   const totalPrice = cart.cartItems.reduce(
     (accumulator, current) => accumulator + current.price * current.quantity,
