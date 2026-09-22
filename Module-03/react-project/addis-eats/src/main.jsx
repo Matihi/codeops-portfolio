47dc0b8e4f5efc6f64f83f1f8dd96a5d89a57d5d
Matihi <mesfinnatitik@gmail.com>
2026-09-23 01:17:17 +0300

    remove cart context provider wrapper from App root

--- Module-03/react-project/addis-eats/src/main.jsx
+++ Module-03/react-project/addis-eats/src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import CartProvider from "./context/cart/CartProvider.jsx";
 import AuthProvider from "./context/authentication/AuthProvider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <App />
     </AuthProvider>
   </StrictMode>,
 );
